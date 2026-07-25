8fbc4d6c5befda27e63959e7bd64f0a8520a475b
Cici <runxi0626@gmail.com>
2026-07-24 21:47:04 -0700

    Initial Commit

---
+++ Calm Capy 2026/Calm_Capy_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  Calm_Capy_2026App.swift
+//  Calm Capy 2026
+//
+//  Created by Cici Xing on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Calm_Capy_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calm Capy 2026Tests/Calm_Capy_2026Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Calm_Capy_2026Tests.swift
+//  Calm Capy 2026Tests
+//
+//  Created by Cici Xing on 7/24/26.
+//
+
+import Testing
+@testable import Calm_Capy_2026
+
+struct Calm_Capy_2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
